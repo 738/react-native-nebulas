e2fe5688d0483a84247ad1aad8cdc26cd2ba45a5
junwooji <junwoo@finda.co.kr>
2018-10-16 00:36:40 +0900

    initial commit

---
+++ ios/RNReactNativeNebulas.m
@@ -0,0 +1,13 @@
+
+#import "RNReactNativeNebulas.h"
+
+@implementation RNReactNativeNebulas
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+RCT_EXPORT_MODULE()
+
+@end
+  
